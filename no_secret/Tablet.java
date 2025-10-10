d998b98a9d897b5346a3bf8cbd1ecb88c2fed944
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-10-10 13:17:16 +0500

    no_secret/Tablet.java

---
+++ no_secret/Tablet.java
@@ -0,0 +1,11 @@
+// Файл: no_secret/Tablet.java
+package no_secret;
+
+import devices.AbstractDevice;
+
+public class Tablet {
+    public void testAccess() {
+        AbstractDevice device = new AbstractDevice("Model Z") {}; 
+        //device.showModel(); // Ошибка: метод защищён
+    }
+}
