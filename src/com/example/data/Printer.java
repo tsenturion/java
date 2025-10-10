f745b9de400cbf25ef3d5d4229e5f71565300bc5
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-10-10 12:39:44 +0500

    default package-private

---
+++ src/com/example/data/Printer.java
@@ -0,0 +1,10 @@
+// Файл: src/com/example/data/Printer.java
+
+package src.com.example.data;
+
+public class Printer {
+    public static void main(String[] args) {
+        Data data = new Data();
+        System.out.println("x = " + data.x); // доступ разрешен
+    }
+}
---
+++ src/com/example/test/Tester.java
@@ -0,0 +1,10 @@
+// Файл: src/com/example/data/Printer.java
+
+package src.com.example.test;
+
+public class Tester {
+    public static void main(String[] args) {
+        Data data = new Data();
+        System.out.println(data.x); // ошибка!
+    }
+}
