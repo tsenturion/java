dec4b1333f7f8189fe57cfb76d86303e28db5ca3
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-10-10 16:10:55 +0500

    pz Person done

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
+class Person {
+    String name; // Поле для хранения имени
+
+    // Метод для вывода информации
+    void displayInfo() {
+        System.out.println("Имя: " + name); // Вывод имени на экран
+    }
+}
+
 public class Main {
+    // Точка входа для проверки
     public static void main(String[] args) {
-        System.out.println("Программа запущена!");
+        Person p1 = new Person(); // Создаем объект класса Person
+        p1.name = "Иван";         // Присваиваем значение полю name
+        p1.displayInfo();         // Вызываем метод для вывода информации
     }
 }
