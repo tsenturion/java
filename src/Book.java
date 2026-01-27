3bd293fe10467252b200bac69845344b4fd93809
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-01-27 14:54:05 +0500

    Задача 4: Класс «книга» и «библиотека»

---
+++ src/Book.java
@@ -0,0 +1,14 @@
+/*
+Создайте класс Book с полями: title, author, year, isAvailable (доступна ли
+для выдачи).
+Создайте класс Library, который хранит массив книг.
+Реализуйте методы:
+• addBook(Book book) — добавить книгу.
+• borrowBook(String title) — взять книгу (если доступна).
+• returnBook(String title) — вернуть книгу.
+• listAvailableBooks() — вывести доступные книги.
+*/
+
+public class Book {
+    
+}
